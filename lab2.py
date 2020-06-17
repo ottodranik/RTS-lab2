1df72da0030e7ee9386771afabcdd6e3f67d6ad5
ottodranik <fanjuve@gmail.com>
2020-06-17 20:50:59 +0300

    Measure the time of corellations

--- lab2.py
+++ lab2.py
@@ -8,8 +8,13 @@ import random
 import math
 import numpy as np
 import pandas as pd
+import time
 
 from lab1 import generate_signal, draw, DrawOption, N, n, w, A, x
+
+
+Rxx_timer = []
+Rxy_timer = []
 
 def expected_value(signal):
   M = 0
@@ -28,19 +33,21 @@ def dispersion(M, signal):
 
 # Автокорелляция сигнала №1 на промежутке tt = -N..N
 def autocorelation(signal, M):
+  start_time = time.time()
   Rxx = []
   for tt in range(0, N):
     rx = 0
     for t in range(N):
-      # xtt = signal[t+tt] if len(signal) > t+tt else 0
       if t+tt > 0 and t+tt < N:
         rx += (signal[t] - M) * (signal[t+tt] - M)
     rx = rx / (N - 1)
     Rxx.append(rx)
+  Rxx_timer.append(time.time() - start_time)
   return Rxx
 
 # Взаимная корелляция на промежутке tt = 0..N/2
 def bothcorelation(signal1, M1, signal2, M2):
+  start_time = time.time()
   Rxy = []
   for tt in range(0, N):
     ry = 0
@@ -49,23 +56,31 @@ def bothcorelation(signal1, M1, signal2, M2):
         ry += (signal1[t] - M1) * (signal2[t+tt] - M2)
     ry = ry / (N - 1)
     Rxy.append(ry)
+  Rxy_timer.append(time.time() - start_time)
   return Rxy
 
 def main_fn():
   signal1 = np.array([generate_signal(i) for i in range(N)])
   M1 = expected_value(signal1)
   D1 = dispersion(M1, signal1)
-  print('Mat №1: ', M1)
-  print('D №1: ', D1)
+  # print('Mat №1: ', M1)
+  # print('D №1: ', D1)
 
   signal2 = np.array([generate_signal(i) for i in range(N)])
   M2 = expected_value(signal2)
   D2 = dispersion(M2, signal2)
-  print('Mat №2: ', M2)
-  print('D №2: ', D2)
+  # print('Mat №2: ', M2)
+  # print('D №2: ', D2)
 
-  Rxx = autocorelation(signal1, M1)
-  Rxy = bothcorelation(signal1, M1, signal2, M2)
+  for _ in range(0, 2000):
+    Rxx = autocorelation(signal1, M1)
+    Rxy = bothcorelation(signal1, M1, signal2, M2)
+  
+  print("Rxx: --- %s seconds ---" % sum(Rxx_timer))
+  print("Rxy: --- %s seconds ---" % sum(Rxy_timer))
+
+  print("Rxx average: --- %s seconds ---" % (sum(Rxx_timer) / 2000))
+  print("Rxy average: --- %s seconds ---" % (sum(Rxy_timer) / 2000))
 
   options = [
     DrawOption("Signal 1", "plot"),
